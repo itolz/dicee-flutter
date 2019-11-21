15d68a97e6ddf2e2752aa26f8d5ba9f04edff412
Italo Vinicios <italovinicios@gmail.com>
2019-11-21 19:49:09 -0300

    uso basico de variável

--- lib/main.dart
+++ lib/main.dart
@@ -18,20 +18,22 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var leftDiceNumber = 5;
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              onPressed: (){
+              onPressed: () {
                 print('left button was pressed.');
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              onPressed: (){
+              onPressed: () {
                 print('right button was pressed.');
               },
               child: Image.asset('images/dice1.png'),
